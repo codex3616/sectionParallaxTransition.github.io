873a4b988df918bfede58391671b22115cbd346f
codeX3616 <aakkuu3616@gmail.com>
2024-06-25 08:31:22 +0530

    project done

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,53 @@
+"use client";
+import React, { useState } from "react";
 import styles from "./page.module.css";
+import Project from "../components/project/index";
+import Modal from "../components/modal/index";
 
-export default function Home() {
-  return <main></main>;
-}
+const Page = () => {
+  const [modal, setModal] = useState({ active: false, index: 0 });
+  const projects = [
+    {
+      title: " BurgerZest",
+      src: "project.png",
+      color: "#000000",
+    },
+    {
+      title: " Login App",
+      src: "project1.png",
+      color: "#8C8C8C",
+    },
+    {
+      title: "MoveInfo",
+      src: "project2.png",
+      color: "#EFE8D3",
+    },
+    {
+      title: " NoteTaking",
+      src: "project3.png",
+      color: "#706D63",
+    },
+  ];
+
+  return (
+    <>
+      <main className={styles.main}>
+        <div className={styles.body}>
+          {projects.map((project, index) => {
+            return (
+              <Project
+                key={index}
+                index={index}
+                title={project.title}
+                setModal={setModal}
+              />
+            );
+          })}
+        </div>
+        <Modal modal={modal} projects={projects} />
+      </main>
+    </>
+  );
+};
+
+export default Page;
