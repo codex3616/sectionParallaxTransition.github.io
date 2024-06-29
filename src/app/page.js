fe8b3c4f024c14bed30bd144a078c9d388ee63d4
codeX3616 <aakkuu3616@gmail.com>
2024-06-29 19:22:18 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,80 +1,86 @@
-import React from "react";
-import styles from "./page.module.scss";
-import Double from "../components/double/index";
-
-const projects = [
-  {
-    name: "Burgerzest",
-    client: "own",
-    description: "Lorem ipsum dolor sit ame",
-    src: "demo.jpeg",
-    year: 2022,
-  },
-
-  {
-    name: "Login Auth",
-    client: "own",
-    description: "Lorem ipsum dolor sit ame",
-    src: "project.png",
-    year: 2021,
-  },
-  {
-    name: "Notetaking",
-    client: "own",
-    description: "Lorem ipsum dolor sit ame",
-    src: "project1.png",
-    year: 2023,
-  },
-  {
-    name: "MovieInfo",
-    client: "own",
-    description: "Lorem ipsum dolor sit ame",
-    src: "project3.png",
-    year: 2024,
-  },
-  {
-    name: "WeatherLive",
-    client: "own",
-    description: "Lorem ipsum dolor sit ame",
-    src: "project2.png",
-    year: 2024,
-  },
-  {
-    name: "DynamicQuotes",
-    client: "own",
-    description: "Lorem ipsum dolor sit ame",
-    src: "project1.png",
-    year: 2024,
-  },
-  {
-    name: "PortFolio",
-    client: "own",
-    description: "Lorem ipsum dolor sit ame",
-    src: "demo1.jpeg",
-    year: 2024,
-  },
-  {
-    name: "DynamicQuotes",
-    client: "own",
-    description: "Lorem ipsum dolor sit ame",
-    src: "demo2.jpeg",
-    year: 2024,
-  },
-];
+"use client";
+import React, { useEffect, useRef } from "react";
+import styles from "./page.module.css";
 
 const Page = () => {
+  const path = useRef(null);
+  let progress = 0;
+  let time = Math.PI / 2;
+  let reqId = null;
+  let x = 0.5;
+
+  useEffect(() => {
+    setPath(progress);
+  });
+
+  const setPath = (progress) => {
+    const { innerWidth } = window;
+    const width = innerWidth * 0.7; // here 0.7 taken as i gave width to main is 70% and now this width variable has actual width of line so we can use it
+    path.current.setAttributeNS(
+      "",
+      "d",
+      `M0 50 Q${width * x} ${50 + progress}, ${width} 50`
+    ); // this will create line
+  };
+
+  const manageMouseEnter = () => {
+    if (reqId) {
+      window.cancelAnimationFrame(reqId);
+      resetAnimation();
+    }
+  };
+
+  const manageMouseMove = (e) => {
+    const { movementY, clientX } = e;
+    const { left, width } = path.current.getBoundingClientRect();
+    x = (clientX - left) / width;
+    progress += movementY;
+    setPath(progress);
+  };
+  const manageMouseLeave = () => {
+    // this is for animate on mouse leave
+    animateOut();
+  };
+
+  const lerp = (x, y, a) => x * (1 - a) + y * a;
+
+  const animateOut = () => {
+    const newProgress = progress * Math.sin(time);
+    time += 0.2;
+    setPath(newProgress);
+    progress = lerp(progress, 0, 0.025);
+
+    if (Math.abs(progress) > 0.75) {
+      reqId = window.requestAnimationFrame(animateOut);
+    } else {
+      resetAnimation();
+    }
+  };
+
+  const resetAnimation = () => {
+    time = Math.PI / 2;
+    progress = 0;
+  };
   return (
     <>
       <main className={styles.main}>
-        <h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veniam
-          maxime perferendis deleniti.
-        </h1>
-        <div className={styles.gallery}>
-          <Double projects={[projects[0], projects[1]]} />
-          <Double projects={[projects[2], projects[3]]} reversed={true} />
-          <Double projects={[projects[4], projects[5]]} />
-          <Double projects={[projects[6], projects[7]]} reversed={true} />
+        <h1>This is vanilla line animation</h1>
+        <div className={styles.line}>
+          <div
+            className={styles.box}
+            onMouseEnter={() => {
+              manageMouseEnter();
+            }}
+            onMouseMove={(e) => {
+              manageMouseMove(e);
+            }}
+            onMouseLeave={() => {
+              manageMouseLeave();
+            }}
+          ></div>
+          <svg>
+            <path ref={path}></path>
+          </svg>
         </div>
       </main>
     </>
