7d4f0dab578689e70dcaba66dbf2dd02ab70137a
codeX3616 <aakkuu3616@gmail.com>
2024-07-08 14:34:30 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./page.module.scss";
-import ZoomParallax from "./components/ZoomParallax";
 import Lenis from "@studio-freight/lenis";
+import Image from "next/image";
+import img from "../../public/images/demo.jpeg";
+import { useScroll, motion, useTransform } from "framer-motion";
 
 const Page = () => {
+  const container = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
   useEffect(() => {
     const lenis = new Lenis();
 
@@ -15,12 +23,38 @@ const Page = () => {
 
     requestAnimationFrame(raf);
   }, []);
+
   return (
     <>
-      <main className={styles.main}>
-        <ZoomParallax />
+      <main className={styles.main} ref={container}>
+        <Section1 scrollYProgress={scrollYProgress} />
+        <Section2 scrollYProgress={scrollYProgress} />
       </main>
     </>
+  );
+};
+
+const Section1 = ({ scrollYProgress }) => {
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, -5]);
+
+  return (
+    <motion.div style={{ scale: scale, rotate: rotate }} className={styles.box}>
+      <h1>srolling section parallax transition</h1>
+    </motion.div>
+  );
+};
+const Section2 = ({ scrollYProgress }) => {
+  const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [-5, 0]);
+
+  return (
+    <motion.div
+      className={styles.imgBox}
+      style={{ scale: scale, rotate: rotate }}
+    >
+      <Image alt="img" fill placeholder="blur" src={img} />
+    </motion.div>
   );
 };
 
