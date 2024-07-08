784b3403e86eec4d39a987b97a6ebd30bb07a493
codeX3616 <aakkuu3616@gmail.com>
2024-07-08 13:45:54 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,24 @@
-// "use client";
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import styles from "./page.module.scss";
-import Text from "./components/Text";
-import Scene from "./components/Scene";
+import ZoomParallax from "./components/ZoomParallax";
+import Lenis from "@studio-freight/lenis";
 
 const Page = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <>
       <main className={styles.main}>
-        <Text />
-        <Scene />
+        <ZoomParallax />
       </main>
     </>
   );
